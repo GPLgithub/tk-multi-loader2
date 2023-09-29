43b5aeb71149b6ca78b8a100b75da31acec696df
ksallee <57231302+ksallee@users.noreply.github.com>
2023-09-29 09:22:45 -0600

    For #11530: Added settings to allow more control over the publish history

--- python/tk_multi_loader/model_publishhistory.py
+++ python/tk_multi_loader/model_publishhistory.py
@@ -64,19 +64,17 @@ class SgPublishHistoryModel(ShotgunModel):
             publish_type_field = "tank_type"
 
         # fields to pull down
-        fields = [publish_type_field] + constants.PUBLISHED_FILES_FIELDS
+        fields = [publish_type_field] + constants.PUBLISHED_FILES_FIELDS + app.get_setting("published_file_fields", [])
 
-        # when we filter out which other publishes are associated with this one,
+        # When we filter out which other publishes are associated with this one,
         # to effectively get the "version history", we look for items
-        # which have the same project, same entity assocation, same name, same type
-        # and the same task.
-        filters = [
-            ["project", "is", sg_data["project"]],
-            ["name", "is", sg_data["name"]],
-            ["task", "is", sg_data["task"]],
-            ["entity", "is", sg_data["entity"]],
-            [publish_type_field, "is", sg_data[publish_type_field]],
-        ]
+        # based on the publish_history_group_by_fields setting.
+        # By default, it finds Published Files with the same
+        # project, task, entity, name and published_file_type.
+        group_by_fields = app.get_setting("publish_history_group_by_fields", [])
+        filters = []
+        for field in group_by_fields:
+            filters.append([field, "is", sg_data[field]])
 
         # add external filters from config
         app = sgtk.platform.current_bundle()
